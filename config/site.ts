e479927093e58ee170dbf51bdf6e4bb12d9823fe
prabirstack <prabirsacademy@gmail.com>
2024-04-06 23:19:22 +0530

    stage 3 site header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: 'PrabirBlog',
+  url: 'https://example.com',
+  description: 'Nextjs 14 blog using velite, tailwind and shadcn',
+  author: 'Prabir Singh',
+  links: {
+    twitter: 'https://twitter.com/',
+    github: 'https://github.com/prabirstack',
+    personalSite: 'https://www.kemiadeleke.com/',
+  },
+}
+
+export type SiteConfig = typeof siteConfig
